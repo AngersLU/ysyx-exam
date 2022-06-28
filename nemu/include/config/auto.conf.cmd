211d54493c56733cf9237c3ab15847d924bb34d1
tracer-ics2021 <tracer@njuics.org>
2022-06-28 11:25:56 +0800

    >  compile 220010    Static hostname: XD          Icon name: computer-laptop            Chassis: laptop         Machine ID: a1e94551632d4941a50d1d4b229c534c            Boot ID: bc2124c615e1458ebb56b66d6b378f6a   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-48-generic       Architecture: x86-64  11:25:56 up  3:00,  1 user,  load average: 0.38, 0.52, 0.50

--- nemu/include/config/auto.conf.cmd
+++ nemu/include/config/auto.conf.cmd
@@ -1,7 +1,7 @@
 deps_config := \
 	src/device/Kconfig \
 	src/memory/Kconfig \
-	/home/lff/ysyx-workbench/nemu/Kconfig
+	/home/lff/Desktop/ysyx-workbench/nemu/Kconfig
 
 include/config/auto.conf: \
 	$(deps_config)
