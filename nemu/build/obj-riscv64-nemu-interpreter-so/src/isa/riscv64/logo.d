2b44815e6da076d62bf27ac0e3946c8688326630
tracer-ics2021 <tracer@njuics.org>
2022-07-19 16:36:15 +0800

    >  compile 220010    Static hostname: XD          Icon name: computer-laptop            Chassis: laptop         Machine ID: a1e94551632d4941a50d1d4b229c534c            Boot ID: e14765f1332a4505944bfc6d15f69590   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-41-generic       Architecture: x86-64  16:36:15 up 2 days,  2:35,  1 user,  load average: 0.52, 0.43, 0.44

---
+++ nemu/build/obj-riscv64-nemu-interpreter-so/src/isa/riscv64/logo.d
@@ -0,0 +1,9 @@
+cmd_/home/lff/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter-so/src/isa/riscv64/logo.o := unused
+
+source_/home/lff/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter-so/src/isa/riscv64/logo.o := src/isa/riscv64/logo.c
+
+deps_/home/lff/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter-so/src/isa/riscv64/logo.o := \
+
+/home/lff/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter-so/src/isa/riscv64/logo.o: $(deps_/home/lff/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter-so/src/isa/riscv64/logo.o)
+
+$(deps_/home/lff/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter-so/src/isa/riscv64/logo.o):
